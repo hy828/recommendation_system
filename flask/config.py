f5a341d05ae78dd4f96a4fce0875df839099110a
liuhanyan <hanyen0828@gmail.com>
2024-08-07 20:27:20 +0800

    update readme

--- flask/config.py
+++ flask/config.py
@@ -1,13 +1,21 @@
-jwt_secret_key = "rec"
+import os
+from dotenv import load_dotenv
 
-DIALECT = 'mysql'
-DRIVER = 'pymysql'
-USERNAME = 'root'
-PASSWORD = 'qwertyuiop123'
-HOST = '127.0.0.1'
-PORT = '3306'
-DATABASE = 'recommendation_system'
-SQLALCHEMY_DATABASE_URI = '{}+{}://{}:{}@{}:{}/{}?charset=utf8'.format(DIALECT, DRIVER, USERNAME, PASSWORD, HOST, PORT,
-                                                                       DATABASE)
+# Load environment variables from .env file
+load_dotenv()
+
+jwt_secret_key = os.getenv('JWT_SECRET_KEY')
+
+DIALECT = os.getenv('DIALECT')
+DRIVER = os.getenv('DRIVER')
+USERNAME = os.getenv('USERNAME')
+PASSWORD = os.getenv('PASSWORD')
+HOST = os.getenv('HOST')
+PORT = os.getenv('PORT')
+DATABASE = os.getenv('DATABASE')
+
+SQLALCHEMY_DATABASE_URI = '{}+{}://{}:{}@{}:{}/{}?charset=utf8'.format(
+    DIALECT, DRIVER, USERNAME, PASSWORD, HOST, PORT, DATABASE
+)
 
 # DEBUG = True
